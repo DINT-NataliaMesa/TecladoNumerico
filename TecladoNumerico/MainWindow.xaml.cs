85e00804a775dffe04a0addd43fd04d75b704b40
Natalia Mesa <na.mesa@iesdoctorbalmis.com>
2019-10-11 10:09:35 +0200

    Agregar archivos de proyecto.

---
+++ TecladoNumerico/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TecladoNumerico
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CreacionDeNBotonesNumericos(0, 0);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            PantallaTextBlock.Text += ((Button)sender).Tag.ToString();
+        }
+        private void CreacionDeNBotonesNumericos(int desde, int hasta)
+        {
+            //foreach (object obj in Enum.GetValues(typeof(Grid)))
+            //{
+
+            //}
+        }           
+    }
+}
